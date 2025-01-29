19c20a91bfc7a31b8ff5a4ffe2e610eafdccdc05
anirudhys123 <saianirudh643@gmail.com>
2025-01-29 22:02:01 +0530

    Generate all subsets (power set) of a given set using recursion.

---
+++ subset.py
@@ -0,0 +1,11 @@
+def subsets(nums):
+    def backtrack(start, path):
+        result.append(path[:])
+        for i in range(start, len(nums)):
+            path.append(nums[i])
+            backtrack(i + 1, path)
+            path.pop()
+
+    result = []
+    backtrack(0, [])
+    return result
